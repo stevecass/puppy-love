e06a8b9ee92c920cddf3b6a084b97a18e86ff7ae
Jeremy Salazar <jeremysalazar09@gmail.com>
2014-09-19 17:15:39 -0400

    Minor changes to message associations

--- db/migrate/20140919191830_create_messages.rb
+++ db/migrate/20140919191830_create_messages.rb
@@ -1,8 +1,8 @@
 class CreateMessages < ActiveRecord::Migration
   def change
     create_table :messages do |t|
-      t.references :sender, foreign_key: :dog
-      t.references :recipient, foreign_key: :dog
+      t.references :sender, class_name: 'Dog', foreign_key: :dog_id
+      t.references :recipient, class_name: 'Dog', foreign_key: :dog_id
       t.text :content
 
       t.timestamps
