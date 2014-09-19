09380dd9ce63c7cfa2caa448c0733a60ea9ba9c3
Shiraj Ganguly <shirajganguly@gmail.com>
2014-09-19 12:03:08 -0400

    Added welcome page

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,8 @@
+class WelcomeController < ApplicationController
+
+  def index
+
+  end
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+end
