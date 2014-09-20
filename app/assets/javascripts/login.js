ddf28513ab8ef8ef10f4252556868152738fecd2
Victor Yang <victory@Victors-MacBook-Pro.local>
2014-09-20 17:07:21 -0400

    Add javascript for modals

--- app/assets/javascripts/login.js
+++ app/assets/javascripts/login.js
@@ -1,14 +1,36 @@
 $( document ).ready(function() {
 	$('#login').click(function(event){
 		event.preventDefault()
-		$.ajax({
-      url: '/login/',
-      type: 'GET',
-      dataType: 'html'
-    }).done(function(response){
-      console.log(response);
-    }).fail(function(response){
-      console.log('FAIL');
-    });
+		displayLogin()
 	})
+
+	$('#signup').click(function(event){
+		event.preventDefault()
+		displaySignup()
+	})	
+
+	$('#overlay').click(function(event){
+		$('#overlay').css( "display", "none")
+		$('#loginmodal').css( "display", "none")
+		$('#signupmodal').css( "display", "none")
+	})
+
+	if( $('#signuperror').length != 0 ){
+		displaySignup();
+	}
+
+	if( $('#loginerror').length != 0 ){
+		displayLogin();
+	}
 });
+
+function displaySignup(){
+	$('#overlay').css( "display", "inherit")
+	$('#signupmodal').css( "display", "inherit")
+}
+
+function displayLogin(){
+	$('#overlay').css( "display", "inherit")
+	$('#loginmodal').css( "display", "inherit")
+
+}
