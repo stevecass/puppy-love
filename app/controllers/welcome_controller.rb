212a5a2d27bc81ac9637a9f9fc2ec93c4856af49
Shiraj Ganguly <shirajganguly@gmail.com>
2014-09-20 11:02:36 -0400

    Stub out welcome controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,21 @@
 class WelcomeController < ApplicationController
 
   def index
-
+    #show a cool web 2.0 splash page of a dog
   end
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
+
+  def login
+    #user login form
+  end
+
+  def auth
+    #process login form and authorize user
+    #set session info on auth
+  end
+
+  def logout
+    #clear the current session
+    #redirect to welcome#index
+  end
 end
 
