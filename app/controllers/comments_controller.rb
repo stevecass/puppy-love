48079fb3c52393bc4cc6b9f598f336db937fb69a
Victor Yang <victory@Victors-MacBook-Pro.local>
2014-09-20 19:52:48 -0400

    Create wall
    Added wall CSS styling for comments
    Added comments controller
    Added wall and comments partials

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,31 @@
+class MessagesController < ApplicationController
+
+  def index
+    
+  end
+
+  def show
+  
+  end 
+
+  def new
+    
+  end
+
+  def create
+    #create new comment
+  end
+
+  def edit
+    
+  end
+
+  def update
+    #update the contents
+  end
+
+  def destroy
+    #destroy comment
+  end
+
+end
