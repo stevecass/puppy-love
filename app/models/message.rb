90b7e9874ed35ca689fab0efaa056a4514b5849d
Shiraj Ganguly <shirajganguly@gmail.com>
2014-09-19 21:07:48 -0400

    Fix messages associations

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,4 @@
 class Message < ActiveRecord::Base
-  belongs_to :sender, class_name: 'Dog'
-  belongs_to :recipient, class_name: 'Dog'
+  belongs_to :sender, foreign_key: "sender_id", class_name: 'Dog'
+  belongs_to :recipient, foreign_key: "recipient_id", class_name: 'Dog'
 end
