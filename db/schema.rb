a7b95e1d7d91c241b5881d5e0180a5a646efe5dc
Nick Gio <ngiovacchini@gmail.com>
2014-09-21 10:43:19 -0400

    Add Search table to schema

--- db/schema.rb
+++ db/schema.rb
@@ -57,4 +57,9 @@ ActiveRecord::Schema.define(version: 20140919200744) do
     t.datetime "updated_at"
   end
 
+  create_table "searches", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
 end
