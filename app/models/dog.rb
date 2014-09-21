b37183d5e71010aadc652eca16a31e9ca719f955
Nick Gio <ngiovacchini@gmail.com>
2014-09-21 10:41:53 -0400

    Add Filterrific logic to Dog Model

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -5,4 +5,38 @@ class Dog < ActiveRecord::Base
   has_many :received_messages, class_name: 'Message', foreign_key: :recipient_id
   has_many :comments, as: :commentable
   has_many :sent_comments, class_name: 'Comment', foreign_key: :author_id
+
+ filterrific(
+   default_settings: { sorted_by: 'created_at_desc' },
+   filter_names: [
+     :sorted_by,
+     :search_query,
+     :with_birthday
+   ]
+ )
+
+ # Scope definitions. We implement all Filterrific filters through ActiveRecord
+ # scopes. In this example we omit the implementation of the scopes for brevity.
+ # Please see 'Scope patterns' for scope implementation details.
+ scope :search_query, lambda { |query|
+   # Filters dogs whose name matches the query
+   #...
+ }
+ scope :sorted_by, lambda { |sort_key|
+   # Sorts dogs by sort_key
+   #...
+ }
+ scope :with_created_at_gte, lambda { |ref_date|
+   #...
+ }
+
+
+ def self.options_for_sorted_by
+   [
+     ['Name (a-z)', 'name_asc'],
+     ['Age (newest first)', 'created_at_desc'],
+     ['Age (oldest first)', 'created_at_asc'],
+   ]
+ end
+
 end
