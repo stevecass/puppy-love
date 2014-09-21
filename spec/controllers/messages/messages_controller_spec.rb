110a7f4282f37845cefe5ae3c95c7619c2234999
Jeremy Salazar <jeremysalazar09@gmail.com>
2014-09-21 14:16:26 -0400

    Some tests passing for messages controller, but three still are not passing

--- spec/controllers/messages/messages_controller_spec.rb
+++ spec/controllers/messages/messages_controller_spec.rb
@@ -1,34 +1,37 @@
 require 'rails_helper'
+require 'spec_helper'
 
 describe MessagesController, type: :controller do
 
   let(:user){Owner.create(email: 'test@test.com', password: 'test')}
 
-  before(:each) do 
+  before(:each) do
     session[:current_user] = user.id
   end
 
 
-  describe "#index" do 
+  describe "#index" do
     it "Should show all sent and received messages" do
+      user_one = Owner.create(email: 'testy@test.com', password: 'test')
       scruffy = Dog.create(name: 'Scruffy')
       zoey = Dog.create(name: 'Zoey')
-      user.dogs << zoey
+      user_one.dogs << zoey
       message_to = Message.create(sender_id: zoey.id, recipient_id: scruffy.id, content: "Hi you.")
       message_from = Message.create(sender_id: scruffy.id, recipient_id: zoey.id, content: "Hello to you too.")
-      
+
       get :index
-      expect #there to be divs with the message ids specific to those two?
+      expect(response.body).to have_css('div#inbox div#received_message')
+      expect(response.body).to have_css('div#outbox div#sent_message')
     end
-    it "Should show the 'no messages' text if there aren't any messages" do 
+    it "Should show the 'no messages' text if there aren't any messages" do
       get :index
-      expect #the inbox div to have the text for no messages
-      expect #the sent messages div to have the text for no messages
+      expect(response.body).to have_css('div#inbox p', :text => "You have 0 messages in your inbox.")
+      expect(response.body).to have_css('div#outbox p', :text => "You have not sent a message.")
     end
   end
 
 
-describe "#new" do 
+describe "#new" do
     it "Should show the new message form" do
       get :new
       expect(response).to render_template('new')
@@ -36,31 +39,43 @@ describe "#new" do
   end
 
 
-  describe "#create" do 
+  describe "#create" do
     it "Should create a new message and redirect to the index" do
-      post :create, message: { 
-        sender_id: 1,
-        recipient_id: 2,
+      scruffy = Dog.create(name: 'Scruffy')
+      zoey = Dog.create(name: 'Zoey')
+
+      post :create, message: {
+        sender_id: scruffy.id,
+        recipient_id: zoey.id,
         content: "Hey there, Buddy!"
       }
       expect(response).to redirect_to '/messages'
     end
   end
 
-  describe "#show" do 
-    it "Should show the content" do 
-      pending
+  describe "#show" do
+    user_two = Owner.create(email: 'test2@test.com', password: 'test')
+    scruffy = Dog.create(name: 'Scruffy')
+    zoey = Dog.create(name: 'Zoey')
+    user_two.dogs << zoey
+    message = Message.create(sender_id: zoey.id, recipient_id: scruffy.id, content: "Hi you.")
+    it "Should show the content" do
+      redirect_to '/messages/1'
+      expect(response.body).to have_content(message.content)
     end
-    it "Should have a delete button" do 
-      pending
+    it "Should have a delete button" do
+      redirect_to '/messages/1'
+      expect(response.body).to have_css('form')
     end
+  end
 
-  describe "#destroy" do 
+  describe "#destroy" do
     it "Should delete a message" do
       message_one = Message.create(sender_id: 1, recipient_id: 2, content: "Hi you.")
       message_two = Message.create(sender_id: 2, recipient_id: 1, content: "Hello to you too.")
-      delete :destroy, id: message_two.id 
+      delete :destroy, id: message_two.id
       expect(response).to redirect_to '/messages'
       expect(Message.last.id).to eq(message_one.id)
     end
   end
+end
