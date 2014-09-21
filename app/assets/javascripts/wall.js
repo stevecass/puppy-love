d2abb157054a7153876e227abc9fad1119c760ae
Victor Yang <victory@Victors-MacBook-Pro.local>
2014-09-20 21:11:27 -0400

    Create javascript to prepend comment after posted on wall

--- app/assets/javascripts/wall.js
+++ app/assets/javascripts/wall.js
@@ -1,3 +1,15 @@
-$( document ).ready(function() {
-
-}
+$(document).ready(function(){
+	$('form#newcomments').submit(function(event){
+		event.preventDefault();
+		$.ajax({
+      url: '/comments/',
+      type: 'POST',
+      data: $(this).serialize(),
+      dataType: 'html'
+    }).done(function(response){
+      $('.comments').prepend('<li id="level1">'+response+'</li>')
+    }).fail(function(response){
+      console.log('FAIL');
+    });
+	})
+})
