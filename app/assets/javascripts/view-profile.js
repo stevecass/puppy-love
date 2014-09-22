74e6193f26164dd21075c3eb4698869460e63caf
Nick Gio <ngiovacchini@gmail.com>
2014-09-22 09:55:14 -0400

    Change jQuery to Render Modal for every card

--- app/assets/javascripts/view-profile.js
+++ app/assets/javascripts/view-profile.js
@@ -1,60 +1,19 @@
 $( document ).ready(function() {
 	$('.view-profile').click(function(event){
 		event.preventDefault();
-		displayProfile();
+		displayProfile(event.target);
 	})
 
 	$('#profile-overlay').click(function(event){
 		$('#profile-overlay').css( "display", "none");
-		$('#profilemodal').css( "display", "none");
+		$('.profile_modal').css( "display", "none");
 	})
 
 })
 
 
-function displayProfile(){
+function displayProfile(button){
+	var dogId = $(button).attr('data-dog-id');
 	$('#profile-overlay').css( "display", "inherit");
-	$('#profilemodal').css( "display", "inherit");
-}
-
-
-
-
-$( document ).ready(function() {
-	$('#login').click(function(event){
-		event.preventDefault();
-		displayLogin();
-	})
-
-	$('#signup').click(function(event){
-		event.preventDefault();
-		displaySignup();
-	})	
-
-	$('#overlay').click(function(event){
-		$('#overlay').css( "display", "none");
-		$('#loginmodal').css( "display", "none");
-		$('#signupmodal').css( "display", "none");
-		$('#loginerror').remove();
-		$('#signuperror').remove();
-	})
-
-	if( $('#signuperror').length != 0 ){
-		displaySignup();
-	}
-
-	if( $('#loginerror').length != 0 ){
-		displayLogin();
-	}
-})
-
-function displaySignup(){
-	$('#overlay').css( "display", "inherit");
-	$('#signupmodal').css( "display", "inherit");
-}
-
-function displayLogin(){
-	$('#overlay').css( "display", "inherit");
-	$('#loginmodal').css( "display", "inherit");
-
+	$('#profilemodal-' + dogId).css( "display", "inherit");
 }
